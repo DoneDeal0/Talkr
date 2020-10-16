dcf3feb793b3cd99946361e4d80e7c574dfa5ddd
antoine lanoe <antoine.lanoe@linkfluence.com>
2020-10-16 18:25:57 +0200

    feat: improve type

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { createContext, useState, useContext, useMemo } from "react";
+import { createContext, useState, useContext } from "react";
 
 export interface TalkrProps {
   children: React.ReactNode;
   languages: { [key: string]: {} };
-  mainLanguage?: string;
+  defaultLanguage: string;
+  detectBrowserLanguage?: boolean;
 }
 
 export interface TProps {
@@ -16,17 +17,29 @@ export interface TalkrContext {
   locale: string;
   setLocale: (language: string) => void;
   languages: { [key: string]: {} };
-  mainLanguage: string;
+  defaultLanguage: string;
 }
 
-const defaultLanguage = navigator.language.split("-")[0];
-const TalkrContext = createContext<any>(null);
+const browserLanguage = navigator.language.split("-")[0];
+const TalkrContext = createContext<TalkrContext>({
+  locale: "",
+  setLocale: () => null,
+  languages: {},
+  defaultLanguage: "",
+});
 
-export function Talkr({ children, languages, mainLanguage }: TalkrProps) {
-  const [locale, setLocale] = useState(mainLanguage || defaultLanguage);
+export function Talkr({
+  children,
+  languages,
+  defaultLanguage,
+  detectBrowserLanguage,
+}: TalkrProps) {
+  const [locale, setLocale] = useState(
+    detectBrowserLanguage ? browserLanguage : defaultLanguage
+  );
   return (
     <TalkrContext.Provider
-      value={{ locale, setLocale, languages, mainLanguage }}
+      value={{ locale, setLocale, languages, defaultLanguage }}
     >
       {children}
     </TalkrContext.Provider>
@@ -37,35 +50,33 @@ export function t(
   key: TProps["key"],
   params?: TProps["params"]
 ): string | null {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { locale, languages, mainLanguage } = useContext<TalkrContext>(
+  const { locale, languages, defaultLanguage } = useContext<TalkrContext>(
     TalkrContext
   );
-  const currentLocale = !languages[locale] ? mainLanguage : locale;
+  const currentLocale = !languages[locale] ? defaultLanguage : locale;
+  let result = languages[currentLocale] as TalkrProps["languages"];
   let currentKey = key;
-  if (params && Object.keys(params).includes("count")) {
-    //@ts-ignore
+  if (params && params.count) {
     currentKey = params.count > 1 ? `${key}.plural` : `${key}.default`;
   }
-  let result = languages[currentLocale];
   currentKey.split(".").forEach((k) => {
-    //@ts-ignore
     if (!result[k]) return;
-    //@ts-ignore
     return (result = result[k]);
   });
   if (typeof result !== "string") return null;
   const currentParams =
     params &&
-    Object.entries(params).reduce(
+    (Object.entries(params).reduce(
       (acc, cur) => ({ ...acc, [cur[0]]: cur[1].toString() }),
       {}
-    );
+    ) as TProps["params"]);
   return currentParams
     ? result
-        .split("__")
         //@ts-ignore
-        .map((word) => (currentParams[word] ? currentParams[word] : word))
+        .split("__")
+        .map((word: string) =>
+          currentParams[word] ? currentParams[word] : word
+        )
         .join("")
     : result;
 }
