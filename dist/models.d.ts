b6e141685dbf46aa4018cbedc2dc1bf2327c0457
antoine lanoe <antoine.lanoe@linkfluence.com>
2022-12-02 11:17:09 +0100

    bump dev-dependencies

--- dist/models.d.ts
+++ dist/models.d.ts
@@ -11,16 +11,16 @@ export interface TContext {
     languages: Record<string, any>;
     defaultLanguage: string;
 }
-export declare type TrContext = Omit<TContext, "setLocale">;
-declare type KeyPrefix<T extends string> = T extends "" ? "" : `.${T}`;
-export declare type KeyPath<T> = (T extends object ? {
+export type TrContext = Omit<TContext, "setLocale">;
+type KeyPrefix<T extends string> = T extends "" ? "" : `.${T}`;
+export type KeyPath<T> = (T extends object ? {
     [K in Exclude<keyof T, symbol>]: `${K}${KeyPrefix<KeyPath<T[K]>>}`;
 }[Exclude<keyof T, symbol>] : "") extends infer D ? Extract<D, string> : never;
-export declare type TParams = {
+export type TParams = {
     count?: number;
     [key: string]: any;
 };
-export declare type Autocomplete<schema> = KeyPath<schema> | (string & {});
+export type Autocomplete<schema> = KeyPath<schema> | (string & {});
 export interface UseT extends TContext {
     T: <Key extends string, Params extends TParams>(key: Key, params?: Params) => string;
 }
